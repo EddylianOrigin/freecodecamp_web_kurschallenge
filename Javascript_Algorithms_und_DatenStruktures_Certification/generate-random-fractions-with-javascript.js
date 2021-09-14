0f6a6f83b41539d03bef7b88fc7496bd902f08a9
Sylvain Eddy <nguesson@rhrk.uni-kl.de>
2021-09-15 00:35:22 +0200

    Kurs_update

---
+++ Javascript_Algorithms_und_DatenStruktures_Certification/generate-random-fractions-with-javascript.js
@@ -0,0 +1,10 @@
+function randomFraction() {
+
+    // Only change code below this line
+    if (Math.random() == 0) {
+        return randomFraction();
+    }
+    return Math.random();
+
+    // Only change code above this line
+}
