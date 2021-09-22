bcf9d4f9fce72027ac4087a3ad42d6039f2d4b1b
Sylvain Eddy <nguesson@rhrk.uni-kl.de>
2021-09-22 12:16:04 +0200

    Kurs_update

---
+++ Javascript_Algorithms_und_DatenStruktures_Certification/js Regular expression/match-all-letters-and-numbers.js
@@ -0,0 +1,7 @@
+/**The closest character class in JavaScript to match the alphabet is \w. This shortcut is equal to [A-Za-z0-9_]. This character class matches upper and lowercase letters plus numbers. Note, this character class also includes the underscore character (_). 
+ * Use the shorthand character class \w to count the number of alphanumeric characters in various quotes and strings.
+*/
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
+console.log(result);
---
+++ Javascript_Algorithms_und_DatenStruktures_Certification/js Regular expression/match-ending-string-patterns.js
@@ -0,0 +1,6 @@
+/**You can search the end of strings using the dollar sign character $ at the end of the regex.
+ * Use the anchor character ($) to match the string caboose at the end of the string caboose.
+ */
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);
